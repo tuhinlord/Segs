397a24824e38be3ecd4b41e9bebaec6356851e33
tuhinlord <sayedhosain646@gmail.com>
2024-11-26 02:23:24 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Get references to the buttons and message
+const yesButton = document.getElementById("yesButton");
+const noButton = document.getElementById("noButton");
+const message = document.getElementById("message");
+
+// Function to move the "No" button randomly on the screen
+noButton.addEventListener("click", () => {
+  const maxX = window.innerWidth - noButton.offsetWidth;
+  const maxY = window.innerHeight - noButton.offsetHeight;
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+  noButton.style.position = "absolute";
+  noButton.style.left = `${randomX}px`;
+  noButton.style.top = `${randomY}px`;
+});
+
+// Function to show the message when "Yes" is clicked
+yesButton.addEventListener("click", () => {
+  message.style.display = "block";
+  message.innerHTML = "Yes, I knew you wanted it too. So tonight?";
+});
